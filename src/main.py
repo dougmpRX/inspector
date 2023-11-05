2ce9a7a183b5ef18549a14d6ff42a29ac96c0c4b
Douglas MP <douglasmpestana@gmail.com>
2023-11-05 08:03:38 -0300

    feat: initial script

---
+++ src/main.py
@@ -0,0 +1,24 @@
+import os
+from datetime import datetime
+
+def list_files_tree(directory):
+    file_tree = {}
+    for root, dirs, files in os.walk(directory):
+        relative_path = os.path.relpath(root, directory)
+        file_tree[relative_path] = []
+        for filename in files:
+            file_tree[relative_path].append(filename)
+    return file_tree
+
+def save_tree_to_file(file_tree, output_file):
+    with open(output_file, 'w') as fp:
+        for folder, files in file_tree.items():
+            fp.write(f"{folder}\n")
+            for filename in files:
+                fp.write(f"  +-- {filename}\n")
+
+directory = r'C:/Temp'
+timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+output_file = f"{timestamp}.txt"
+file_tree = list_files_tree(directory)
+save_tree_to_file(file_tree, output_file)
